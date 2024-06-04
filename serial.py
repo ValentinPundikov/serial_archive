5e2c08189c46de8702699decf8ea2288bdc11146
Valentin Pundikov <nkbotcman@gmail.com>
2024-06-04 15:47:19 +0300

    Full

--- serial.py
+++ serial.py
@@ -1,40 +1,58 @@
 import os
-import zipfile
+import tarfile
 import datetime
 import requests
 import logging
 
-# Настройка логгера
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
-file_path = ""
-output_base_path = ""
+logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+logger = logging.getLogger(__name__)
+logger.info("Начало работы скрипта")
+
+file_path = "/etc/point/source/logs/"
+output_base_path = "/etc/point/source/logs/"
 bot_token = ''
 chat_id = ''
 
+
 current_datetime = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-output_zip_path = os.path.join(output_base_path, f'serial_logs_{current_datetime}.zip')
+output_tar_gz_path = os.path.join(output_base_path, f'serial_logs_{current_datetime}.tar.gz')
 
-with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-    for root, dirs, files in os.walk(file_path):
-        for file in files:
-            if file.startswith('serial'):
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, arcname=file)
-                logging.info(f"Архивирую файл: {file_path}")
 
-logging.info(f"Архивация завершена. Результат сохранён в: {output_zip_path}")
+try:
+    with tarfile.open(output_tar_gz_path, "w:gz") as tar:
+        for root, dirs, files in os.walk(file_path):
+            for file in files:
+                if file.startswith('serial'):
+                    file_path = os.path.join(root, file)
+                    # Добавляем файл в архив с корректным именем
+                    tar.add(file_path, arcname=file)
+                    logger.info(f"Архивирую файл: {file_path}")
+    logger.info(f"Архивация завершена. Результат сохранён в: {output_tar_gz_path}")
+except Exception as e:
+    logger.error(f"Ошибка при создании архива: {e}")
 
-url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
-with open(output_zip_path, 'rb') as file:
-    files = {'document': file}
-    response = requests.post(url, files=files, data={'chat_id': chat_id})
 
-if response.status_code == 200:
-    logging.info("Файл успешно отправлен через Telegram.")
-else:
-    logging.error("Ошибка при отправке файла через Telegram.")
+try:
+    url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
+    with open(output_tar_gz_path, 'rb') as file:
+        files = {'document': file}
+        response = requests.post(url, files=files, data={'chat_id': chat_id})
 
-# Удаление ZIP-файла с устройства
-os.remove(output_zip_path)
-logging.info(f"Файл {output_zip_path} удален с устройства.")
+    if response.status_code == 200:
+        logger.info("Файл успешно отправлен через Telegram.")
+    else:
+        logger.error("Ошибка при отправке файла через Telegram.")
+except Exception as e:
+    logger.error(f"Ошибка при отправке файла: {e}")
+
+
+try:
+    os.remove(output_tar_gz_path)
+    logger.info(f"Файл {output_tar_gz_path} удален с устройства.")
+except Exception as e:
+    logger.error(f"Ошибка при удалении файла: {e}")
+
+logger.info("Конец работы скрипта")
