cd65f74a1208ae9a10332fa3a7834f779f7a8a78
Valentin Pundikov <nkbotcman@gmail.com>
2024-06-04 16:02:06 +0300

    Full

--- serial.py
+++ serial.py
@@ -5,7 +5,7 @@ import requests
 import logging
 
 
-logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 logger = logging.getLogger(__name__)
